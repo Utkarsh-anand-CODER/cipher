76e30b17081f07299c3efc7a6d176b0d44fb88a3
Utkarsh Anand <79036968+Utkarsh-anand-CODER@users.noreply.github.com>
2024-05-07 22:45:18 +0530

    Add files via upload

---
+++ xor.py
@@ -0,0 +1,18 @@
+def xor_cipher(text,key):
+    if isinstance(text,str):
+        text=text.encode()
+
+    if len(key)<len(text):
+        key=key*(len(text)//len(key)+1)
+        key=key[:len(text)]
+
+    return bytes([x^y for x,y in zip(text,key)])
+
+original_text="Hello World! Subscribe to Utkarsh"
+
+key=b'Utkarsh'
+encrypted=xor_cipher(original_text,key)
+print(encrypted)
+
+decrypted=xor_cipher(encrypted,key)
+print(decrypted.decode())
